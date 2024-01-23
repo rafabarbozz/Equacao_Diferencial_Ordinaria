dc7ef41be20ac1005e0067989a9adc4ec866846f
Rafael Barboza <rafael.almeida1096@gmail.com>
2024-01-23 12:16:35 -0300

    Adicionando comentários

--- Euler/euler.py
+++ Euler/euler.py
@@ -1,10 +1,11 @@
+# Função para calcular EDO com 1 equação
 def euler_PVI_simples(equacao: str, limite_inf: float, limite_sup: float, qtd_passos: int, y_0: float):
     
-    Vet_x = [None] * (qtd_passos + 1)
-    Vet_y = [None] * (qtd_passos + 1)
+    Vet_x = [None] * (qtd_passos + 1) # Vetor que receberá todos os valores de x
+    Vet_y = [None] * (qtd_passos + 1) # Vetor que receberá todos os valores de y
     Fxy = 0
     
-    h = (limite_sup - limite_inf) / qtd_passos
+    h = (limite_sup - limite_inf) / qtd_passos 
     x = limite_inf
     y = y_0
     
@@ -27,10 +28,12 @@ def euler_PVI_simples(equacao: str, limite_inf: float, limite_sup: float, qtd_pa
     
     return Vet_x, Vet_y
 
+
+# Função para calcular EDO com 2 equações
 def euler_PVI_composto(equacao_1: str, equacao_2: str, limite_inf: float, limite_sup: float, qtd_passos: int, y_10: float, y_20: float):
-    Vet_x = [None] * (qtd_passos + 1)
-    Vet_y1 = [None] * (qtd_passos + 1)
-    Vet_y2 = [None] * (qtd_passos + 1)
+    Vet_x = [None] * (qtd_passos + 1) # Vetor que receberá todos os valores de x
+    Vet_y1 = [None] * (qtd_passos + 1) # Vetor que receberá todos os valores de x
+    Vet_y2 = [None] * (qtd_passos + 1) # Vetor que receberá todos os valores de y2
     Fxy_1 = 0
     Fxy_2 = 0
     
